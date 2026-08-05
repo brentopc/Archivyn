f548a47b19c324a6183bf99a742cef4ca4c8b921
bcroal00 <bcroal00@email.cpcc.edu>
2026-08-04 23:43:02 -0400

    Update Description keyword

---
+++ Migrations/20260805034300_UpdateDescriptionKeyword.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Archivyn.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDescriptionKeyword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "KEYTYPETABLE",
+                keyColumn: "keytypenum",
+                keyValue: 2L,
+                columns: new[] { "AddToAllDocumentTypes", "IsSystem" },
+                values: new object[] { false, true });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "KEYTYPETABLE",
+                keyColumn: "keytypenum",
+                keyValue: 2L,
+                columns: new[] { "AddToAllDocumentTypes", "IsSystem" },
+                values: new object[] { true, false });
+        }
+    }
+}
